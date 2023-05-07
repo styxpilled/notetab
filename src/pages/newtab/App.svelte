a7988c6ba9c3808bc3b90ffffe1f21263b375177
styxpilled <styxpilled.dev@gmail.com>
2023-05-07 22:39:29 +0200

    feat: use bytemd

--- src/pages/newtab/App.svelte
+++ src/pages/newtab/App.svelte
@@ -1,78 +1,48 @@
 <script lang="ts">
-  import PageContent from '~/lib/PageContent.svelte';
-  import browser from 'webextension-polyfill';
-  import { strip, stripHTML } from '~/lib/render';
-  import { CaretUtil } from '~/lib/selection';
-  let timer: ReturnType<typeof setTimeout>;
-  let editor: HTMLElement;
-  let markdown;
-  let savedpos;
+  import 'bytemd/dist/index.css';
+  import { Editor, Viewer } from 'bytemd';
+  import gfm from '@bytemd/plugin-gfm';
 
-  const debounce = () => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      browser.storage.local.set({
-        md: markdown,
-      });
-    }, 500);
-  };
+  let value = '';
+  const plugins = [
+    gfm(),
+    // Add more plugins here
+  ];
 
-  const updatePreview = (
-    event?: Event & { currentTarget: EventTarget & HTMLDivElement },
-    source?: HTMLElement | string
-  ) => {
-    let target = event.currentTarget;
-    source = source ?? target;
-    savedpos = CaretUtil.getCaretPosition(target);
-    markdown = stripHTML(source);
-    console.log(markdown);
-
-    const content = markdown;
-    console.log(content);
-
-    target.innerHTML = content + '<br>';
-    CaretUtil.setCaretPosition(target, savedpos);
-    debounce();
-  };
+  function handleChange(e) {
+    value = e.detail.value;
+  }
 </script>
 
 <main>
-  <PageContent>New Tab</PageContent>
-  <div
-    id="editor"
-    spellcheck="false"
-    contenteditable="true"
-    on:input={updatePreview}
-  >
-    <!-- <pre> -->
-    # heading <br />
-    ## heading2 <br /> <br />
-    other text <br /> **bold**<br />
-    *emphasis*
-    <br />
-    ***bold emphasis*** ~changed my mind~ <br />
-    [ddg link for some reason](https://duckduckgo.com). <br />
-    ![i lovem mywife](https://cdn.discordapp.com/attachments/845778663022395452/968344841140453416/unknown.png)
-    ```js console.log('bals') ```
-    <!-- </pre> -->
+  <div>
+    <Editor {value} {plugins} mode="tab" on:change={handleChange} />
   </div>
 </main>
 
 <style>
-  #editor {
-    padding: 2em 40vh 50vh;
-    background-color: var(--background-primary);
-    font-size: 1.5em;
-    /* background-color: blue; */
-    min-height: 100vh;
-    outline: none;
-    font-family: 'Roboto Mono', sans-serif;
+  :global(html, body) {
+    background-color: aliceblue;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
   }
+
   main {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  div {
+    display: block;
+    width: 50rem;
+    height: 50rem;
+  }
+
+  :global(.bytemd) {
+    height: 50rem;
+    width: 50rem;
   }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,8 @@ export default defineConfig(() => {
         "~": path.resolve(__dirname, "./src"),
       },
     },
+    optimizeDeps: {
+      exclude: ['bytemd']
+    }
   };
 });
