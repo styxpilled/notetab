8e0995052d1083d0bcb1c1bf8e34889803349e4c
styxpilled <styxpilled.dev@gmail.com>
2023-07-07 12:24:52 +0200

    feat: unplugin-icons types

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 /// <reference types="@samrum/vite-plugin-web-extension/client" />
+/// <reference types="unplugin-icons/types/svelte" />
